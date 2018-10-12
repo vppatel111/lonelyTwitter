41c5293bdad3bc4b0002f7c16d965ddb85349119
Vishal Patel <vppatel@ualberta.ca>
2018-10-11 23:36:35 -0600

    Fixed gradle and started on Lab 5 Assignment

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/ElasticsearchTweetController.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/ElasticsearchTweetController.java
@@ -7,68 +7,138 @@ import com.searchly.jestdroid.DroidClientConfig;
 import com.searchly.jestdroid.JestClientFactory;
 import com.searchly.jestdroid.JestDroidClient;
 
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+import io.searchbox.client.JestClient;
+import io.searchbox.client.JestResult;
+import io.searchbox.core.DocumentResult;
 import io.searchbox.core.Index;
+import io.searchbox.core.Search;
 
 /**
  * Created by romansky on 10/20/16.
  */
 public class ElasticsearchTweetController {
-    private static JestDroidClient client;
 
-    // TODO we need a function which adds tweets to elastic search
-    public static class AddTweetsTask extends AsyncTask<NormalTweet, Void, Void> {
+    private static JestDroidClient client = null;
 
-        @Override
-        protected Void doInBackground(NormalTweet... tweets) {
-            //verifySettings();
+    public static class AddTweetTask extends AsyncTask<Tweet, Void, Void> {
 
-            for (NormalTweet tweet : tweets) {
-                Index index = new Index.Builder(tweet).index("testing").type("tweet").build();
+        protected Void doInBackground(Tweet ... params) {
+            setClient();
 
-                try {
-                    // where is the client?
+            Tweet tweet = params[0];
+            Index index = new Index.Builder(tweet)
+                    .index("vishal-wednesday")
+                    .type("tweet").build();
+
+            try {
+                DocumentResult result = client.execute(index);
+                if (result.isSucceeded()) {
+                    tweet.setTweetId(result.getId());
                 }
-                catch (Exception e) {
-                    Log.i("Error", "The application failed to build and send the tweets");
-                }
+            } catch (IOException e) {
 
             }
             return null;
         }
     }
 
-    // TODO we need a function which gets tweets from elastic search
-/*    public static class GetTweetsTask extends AsyncTask<String, Void, ArrayList<NormalTweet>> {
-        @Override
-        protected ArrayList<NormalTweet> doInBackground(String... search_parameters) {
-            verifySettings();
+    public static class GetTweetTask extends AsyncTask<String, Void, ArrayList<Tweet>> {
 
-            ArrayList<NormalTweet> tweets = new ArrayList<NormalTweet>();
+        protected ArrayList<Tweet> doInBackground(String... params) {
+            setClient();
 
-                // TODO Build the query
+            ArrayList<Tweet> tweets = new ArrayList<Tweet>();
+            Search search = new Search.Builder(params[0])
+                    .addIndex("vishal-wednesday")
+                    .addType("tweet")
+                    .build();
 
             try {
-               // TODO get the results of the query
-            }
-            catch (Exception e) {
-                Log.i("Error", "Something went wrong when we tried to communicate with the elasticsearch server!");
+                JestResult result = client.execute(search);
+
+                if (result.isSucceeded()) {
+                    List<NormalTweet> tweetList;
+
+                    // Returns all tweets as a list, but we need to define as a type.
+                    // We can't save into an arrayList because result only gives us a datatype of
+                    // list, we simply add back into the array list.
+                    tweetList = result.getSourceAsObjectList(NormalTweet.class);
+
+                    Log.d("VISHALTAG", "doInBackground: Get successful " + tweetList.size());
+                    tweets.addAll(tweetList);
+                }
+            } catch (IOException e) {
+
             }
 
             return tweets;
         }
-    }*/
 
+    }
 
+    // TODO: Change the index
+    public static class SearchTweetTask extends AsyncTask<String, Void, ArrayList<Tweet>> {
 
+        protected ArrayList<Tweet> doInBackground(String... params) {
+            setClient();
 
-    public static void verifySettings() {
+            ArrayList<Tweet> tweets = new ArrayList<Tweet>();
+
+            String query = "{\n" +
+                    "    \"query\": {\n" +
+                    "        \"filtered\" : {\n" +
+                    "            \"query\" : {\n" +
+                    "                \"query_string\" : {\n" +
+                    "                    \"query\" : \"test\"\n" +
+                    "                }\n" +
+                    "            },\n" +
+                    "            \"filter\" : {\n" +
+                    "                \"term\" : { \"message\" : \"" + params[0] + "\" }\n" +
+                    "            }\n" +
+                    "        }\n" +
+                    "    }\n" +
+                    "}";
+
+            Search search = new Search.Builder(query)
+                    .addIndex("vishal-wednesday")
+                    .addType("tweet")
+                    .build();
+
+            try {
+                JestResult result = client.execute(search);
+
+                if (result.isSucceeded()) {
+                    List<NormalTweet> tweetList;
+
+                    // Returns all tweets as a list, but we need to define as a type.
+                    // We can't save into an arrayList because result only gives us a datatype of
+                    // list, we simply add back into the array list.
+                    tweetList = result.getSourceAsObjectList(NormalTweet.class);
+                    tweets.addAll(tweetList);
+                }
+            } catch (IOException e) {
+
+            }
+
+            return tweets;
+        }
+
+    }
+
+    // Create the JestClient as a singleton.
+    public static void setClient() {
         if (client == null) {
-            DroidClientConfig.Builder builder = new DroidClientConfig.Builder("http://cmput301.softwareprocess.es:8080");
-            DroidClientConfig config = builder.build();
-
+            DroidClientConfig config = new DroidClientConfig
+                    .Builder("http://cmput301.softwareprocess.es:8080/").build(); // Builder pattern.
             JestClientFactory factory = new JestClientFactory();
             factory.setDroidClientConfig(config);
             client = (JestDroidClient) factory.getObject();
         }
     }
+
 }
--- app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
@@ -15,6 +15,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -24,6 +25,8 @@ import android.widget.ListView;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+
+import org.apache.commons.lang3.event.EventListenerSupport;
 
 public class LonelyTwitterActivity extends Activity {
 
@@ -42,7 +45,7 @@ public class LonelyTwitterActivity extends Activity {
 
 		bodyText = (EditText) findViewById(R.id.body);
 		Button saveButton = (Button) findViewById(R.id.save);
-		Button clearButton = (Button) findViewById(R.id.clear);
+		Button searchButton = (Button) findViewById(R.id.searchButton);
 		oldTweetsList = (ListView) findViewById(R.id.oldTweetsList);
 
 		saveButton.setOnClickListener(new View.OnClickListener() {
@@ -52,17 +55,17 @@ public class LonelyTwitterActivity extends Activity {
 				String text = bodyText.getText().toString();
 				Tweet newTweet = new NormalTweet(text);
 				tweetList.add(newTweet);
+				new ElasticsearchTweetController.AddTweetTask().execute(newTweet);
 				adapter.notifyDataSetChanged();
-				saveInFile(); // TODO replace this with elastic search
 			}
 		});
 
-		clearButton.setOnClickListener(new View.OnClickListener() {
+		searchButton.setOnClickListener(new View.OnClickListener() {
 
 			public void onClick(View v) {
 				setResult(RESULT_OK);
 				tweetList.clear();
-				deleteFile(FILENAME);  // TODO deprecate this button
+				new ElasticsearchTweetController.SearchTweetTask().execute(bodyText.getText().toString());
 				adapter.notifyDataSetChanged();
 			}
 		});
@@ -74,45 +77,19 @@ public class LonelyTwitterActivity extends Activity {
 	protected void onStart() {
 		// TODO Auto-generated method stub
 		super.onStart();
-		loadFromFile(); // TODO replace this with elastic search
 		adapter = new ArrayAdapter<Tweet>(this,
 				R.layout.list_item, tweetList);
 		oldTweetsList.setAdapter(adapter);
-	}
 
-
-	private void loadFromFile() {
+		// We need to use "get" for the class to return a string
 		try {
-			FileInputStream fis = openFileInput(FILENAME);
-			BufferedReader in = new BufferedReader(new InputStreamReader(fis));
-			Gson gson = new Gson();
-			//Code taken from http://stackoverflow.com/questions/12384064/gson-convert-from-json-to-a-typed-arraylistt Sept.22,2016
-			Type listType = new TypeToken<ArrayList<NormalTweet>>(){}.getType();
-			tweetList = gson.fromJson(in, listType);
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			tweetList = new ArrayList<Tweet>();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			throw new RuntimeException();
+			tweetList = new ElasticsearchTweetController.GetTweetTask().execute("").get();
+			Log.d("VISHALTAG", "onStart: I got these many posts: " + tweetList.get(1).getMessage());
+			adapter.notifyDataSetChanged();
+		} catch (Exception e) {
+
 		}
+
 	}
 
-
-	private void saveInFile() {
-		try {
-
-			FileOutputStream fos = openFileOutput(FILENAME,0);
-			OutputStreamWriter writer = new OutputStreamWriter(fos);
-			Gson gson = new Gson();
-			gson.toJson(tweetList, writer);
-			writer.flush();
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			throw new RuntimeException();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			throw new RuntimeException();
-		}
-	}
 }
